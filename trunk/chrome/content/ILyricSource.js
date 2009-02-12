fd7abb5db2bfe1fb2e81756b6c5e1429488038bf
loganmoon <loganmoon@bb2bf354-f646-11dd-b556-9305e745af9e>
2009-02-12 18:24:24 +0000

    Added ILyricSource class and util.js file for misc. functions.

---
+++ trunk/chrome/content/ILyricSource.js
@@ -0,0 +1,21 @@
+//Super class of AZLyricSource and DarkLyricSource
+
+
+function ILyricSource(songName, artistName) {
+	this.songName = songName;
+	this.artistName = artistName;
+	this.lyrics = '';
+	this.sourceURL = '';
+}
+
+ILyricSource.method('getLyrics', function () { 
+					return this.lyrics; 
+					});
+
+ILyricSource.method('hasLyrics', function () {
+					return this.lyrics.length != 0;
+					});
+
+ILyricSource.method('getSourceURL', function 90 {
+					return this.sourceURL;
+					});
