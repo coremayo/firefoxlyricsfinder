762b098ec619c3c30cf5beddbf5ab18c4e6a1409
loganmoon <loganmoon@bb2bf354-f646-11dd-b556-9305e745af9e>
2009-02-16 22:25:09 +0000

    Fixed regex in AZLyricsSource

--- chrome/content/AZLyricSource.js
+++ chrome/content/AZLyricSource.js
@@ -7,7 +7,7 @@ function AZLyricSource(songName, artistName)  {
 		searchURL = ('http://search.azlyrics.com/search.php?q=' + artistName + ' ' + songName)
 			.replace(/ /g, '+');
 		search = getPage(searchURL);
-		resultMatch = /<a href=\"([^\"])+\" TARGET=\"_blank\">/.exec(search);
+		resultMatch = (new RegExp("<a href=\"([^\"])+\" TARGET=\"_blank\">")).exec(search);
 		if(resultMatch) {
 			pageURL = $1;
 			page = getPage(pageURL);
