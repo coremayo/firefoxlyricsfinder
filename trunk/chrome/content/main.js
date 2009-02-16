47709d14117a1584580501185580b1f239178068
coremayo <coremayo@bb2bf354-f646-11dd-b556-9305e745af9e>
2009-02-16 22:36:15 +0000

    all javascript is now in chrome/content/main.js updated .xpi file also

--- trunk/chrome/content/main.js
+++ trunk/chrome/content/main.js
@@ -16,7 +16,7 @@ ILyricSource.method('hasLyrics', function () {
 					return this.lyrics.length != 0;
 					});
 
-ILyricSource.method('getSourceURL', function 90 {
+ILyricSource.method('getSourceURL', function () {
 					return this.sourceURL;
 					});
 // Assuming the current tab in firefox is some page on last.fm with a song playing, this function grabs the song title and artist from the title tag and sets the respective labels in the sidebar to those values.
@@ -37,14 +37,14 @@ ILyricSource.method('getSourceURL', function 90 {
 			var basicString = "";
 			basicString = parent.top.document.title;
 			var newString = basicString.split("-",2);
-			return alltrim(newString[1];
+			return alltrim(newString[1]);
 		}
 
 		function getLastFMSongArtist(){
 			var basicString = "";
 			basicString = parent.top.document.title;
 			var newString = basicString.split("-",2);
-			return alltrim(newString[0];
+			return alltrim(newString[0]);
 		}
 
 
