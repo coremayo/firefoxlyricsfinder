6e9a31ad8e63c96697f4f44c33e1288c4a9a823c
loganmoon <loganmoon@bb2bf354-f646-11dd-b556-9305e745af9e>
2009-02-18 22:38:41 +0000

    changed getPage to use XMLHttpRequest

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -98,30 +98,10 @@ AZLyricSource.method('getSourceURL', function () {
 
 // From http://www.rgagnon.com/jsdetails/js-0035.html
 function getPage(url) {
-	pageURL = new	java.net.URL(url);
+	var request =  new XMLHttpRequest();
+	request.open("GET", url, false);
+	request.send(null);
 	
-	// step 1, open the URL
-	var openConnection = pageURL.openConnection;
-	theConnection = openConnection();
 	
-	// step 2, connect to server
-	var t=theConnection.connect;
-	t();
-	
-	// step 3, read the file using HTTP protocol
-	var getContent = theConnection.getContent;
-	var theURLStream = getContent();
-	
-	// step 4, get an handle and fetch the content length
-	var readStream = theURLStream.read;
-	var gcl = theConnection.getContentLength;
-	gcLen = gcl(); 
-	
-	// and finally, read into a variable
-	theText ="";
-	for (i = 1; i <gcLen; i++) {
-		theText += new java.lang.Character(readStream());
-	}
-	
-	return(theText);
+	return(request.responseText);
 }
